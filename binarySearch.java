89c7a17a172d2a849d04ada7c3b17d9e59257960
Raif Momin <161595077+RaifMomin@users.noreply.github.com>
2026-01-26 23:57:28 +0500

    Add files via upload

---
+++ binarySearch.java
@@ -0,0 +1,27 @@
+class binarysearch {
+ public int binary(int arr[], int target) {
+        int left = 0;
+        int right = arr.length -1 ;
+        int mid;
+        while(left<=right){
+            mid=(left+right)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9,10};
+        binarysearch obj = new binarysearch();
+        System.out.println(obj.binary(arr,8));
+
+    }
+}
+
